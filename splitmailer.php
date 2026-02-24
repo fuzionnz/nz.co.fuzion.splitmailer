73c85938a9ca751312d3582471fb9cd8127421ef
Luke Stewart <luke@fuzion.co.nz>
2026-02-25 10:51:34 +1300

    Civix Upgrade

--- splitmailer.php
+++ splitmailer.php
@@ -67,59 +67,12 @@ function splitmailer_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function splitmailer_civicrm_postInstall() {
-  _splitmailer_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function splitmailer_civicrm_uninstall() {
-  _splitmailer_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
 function splitmailer_civicrm_enable() {
   _splitmailer_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function splitmailer_civicrm_disable() {
-  _splitmailer_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function splitmailer_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _splitmailer_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function splitmailer_civicrm_entityTypes(&$entityTypes) {
-  _splitmailer_civix_civicrm_entityTypes($entityTypes);
 }
 
 // --- Functions below this ship commented out. Uncomment as required. ---
